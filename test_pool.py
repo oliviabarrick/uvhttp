bf6b740f894fd62ebbd2c7227bf198b0c4da1082
Your Name <you@example.com>
2017-03-13 00:24:20 +0000

    Add pool and tests for pool

---
+++ test_pool.py
@@ -0,0 +1,186 @@
+import uvhttp.pool
+import asyncio
+import functools
+import time
+
+HEAD = 'HEAD / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n'
+GET_404 = 'GET /lol HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n'
+STATUS_404 = b'HTTP/1.1 404 Not Found'
+STATUS_200 = b'HTTP/1.1 200 OK'
+
+def async_func(func):
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(func(loop))
+
+    return new_func
+
+@async_func
+async def test_connection(loop):
+    pool_available = asyncio.Semaphore(1, loop=loop)
+
+    conn = uvhttp.pool.Connection('127.0.0.1', 80, pool_available, loop)
+
+    await conn.acquire()
+
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    conn.close()
+    conn.release()
+
+@async_func
+async def test_connection_failed(loop):
+    pool_available = asyncio.Semaphore(1, loop=loop)
+
+    conn = uvhttp.pool.Connection('127.0.0.1', 31337, pool_available, loop)
+
+    await conn.acquire()
+
+    try:
+        await conn.send(HEAD)
+        raise AssertionError("ConnectionRefusedError was not raised.")
+    except ConnectionRefusedError:
+        pass
+
+    conn.close()
+    conn.release()
+
+@async_func
+async def test_connection_reuse(loop):
+    pool_available = asyncio.Semaphore(1, loop=loop)
+
+    conn = uvhttp.pool.Connection('127.0.0.1', 80, pool_available, loop)
+    
+    assert not conn.locked()
+    await conn.acquire()
+    assert conn.locked()
+
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    conn.release()
+    assert not conn.locked()
+    await conn.acquire()
+
+    await conn.send(GET_404)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_404)] == STATUS_404
+    assert b'</html>' in response
+
+    conn.release()
+    assert not conn.locked()
+    await conn.acquire()
+
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    conn.close()
+    conn.release()
+
+    assert conn.connect_count == 1
+
+@async_func
+async def test_pool(loop):
+    pool = uvhttp.pool.Pool('127.0.0.1', 80, 2, loop)
+
+    # Open two connections in the pool.
+    conn = await pool.connect()
+    conn2 = await pool.connect()
+
+    # Make sure no connections have actually been established yet.
+    assert await pool.stats() == 0
+
+    # Send a request and confirm the response.
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    # Make sure only one connection has been established so far.
+    assert await pool.stats() == 1
+
+    # Send a request on the other connection.
+    await conn2.send(GET_404)
+    response = await conn2.read(65535)
+    assert response[:len(STATUS_404)] == STATUS_404
+
+    # The pool should have two connections now.
+    assert await pool.stats() == 2
+
+    # Release the connections to the pool.
+    conn.release()
+    conn2.release()
+
+    # Retrieve an old connection from the pool.
+    conn = await pool.connect()
+
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    assert await pool.stats() == 2
+
+    conn.release()
+
+@async_func
+async def test_pool_blocks_when_full(loop):
+    pool = uvhttp.pool.Pool('127.0.0.1', 80, 2, loop)
+
+    # Open two connections in the pool.
+    conn = await pool.connect()
+    conn2 = await pool.connect()
+
+    # Make sure no connections have actually been established yet.
+    assert await pool.stats() == 0
+
+    # Send a request and confirm the response.
+    await conn.send(HEAD)
+    response = await conn.read(65535)
+    assert response[:len(STATUS_200)] == STATUS_200
+
+    # Make sure only one connection has been established so far.
+    assert await pool.stats() == 1
+
+    # Send a request on the other connection.
+    await conn2.send(GET_404)
+    response = await conn2.read(65535)
+    assert response[:len(STATUS_404)] == STATUS_404
+
+    async def block_this(pool):
+        # Make sure it takes 1 second for the pool to be available.
+        start_time = time.time()
+        conn = await pool.connect()
+
+        # Let the main thread know it should try connecting.
+        assert int(time.time() - start_time) == 1
+
+        # Make a request, sleep two seconds, and release the connection for
+        # reuse.
+        await conn.send(GET_404)
+        #response = await conn.read(65535)
+        #assert response[:len(STATUS_404)] == STATUS_404
+        await asyncio.sleep(2)
+
+        conn.release()
+
+    result = asyncio.Event(loop=loop)
+
+    def blocked_cb(future):
+        result.set()
+
+    # Launch a coroutine in the background that is trying to use the pool.
+    blocked = asyncio.ensure_future(block_this(pool), loop=loop)
+    blocked.add_done_callback(blocked_cb)
+
+    # Don't release the pool for one second.
+    await asyncio.sleep(1)
+    conn.release()
+
+    await result.wait()
+    blocked.result()
+
+    assert await pool.stats() == 2
---
+++ uvhttp/pool.py
@@ -0,0 +1,135 @@
+import asyncio
+
+class Connection:
+    """
+    A single connection within a pool. When the acquire() method is called,
+    the connection is locked until release() is called, when it will be
+    released back into the pool.
+    """
+    def __init__(self, host, port, pool_available, loop):
+        self.loop = loop
+
+        # Semaphore used by the Pool to determine if any connections are
+        # available.
+        self.pool_available = pool_available
+        # Lock used to lock the connection, is released by release() to release
+        # the connection back into the pool.
+        self.lock = asyncio.Lock(loop=loop)
+
+        self.reader = None
+        self.writer = None
+
+        self.host = host
+        self.port = port
+
+        # Number of reconnects made. Used to determine pool efficiency.
+        self.connect_count = 0
+
+    async def connect(self):
+        """
+        Open a new connection to the server. Should only be called when the
+        connection has not established yet or we disconnected.
+        """
+        self.connect_count += 1
+        self.reader, self.writer = await asyncio.open_connection(self.host, self.port, loop=self.loop)
+
+    async def read(self, num_bytes):
+        """
+        Read up to num_bytes off of the socket.
+        """
+        if not self.reader:
+            await self.connect()
+
+        return await self.reader.read(num_bytes)
+
+    async def send(self, message):
+        """
+        Write up to num_bytes off of the socket.
+        """
+        if not self.writer:
+            await self.connect()
+
+        self.writer.write(message.encode())
+
+    async def acquire(self):
+        """
+        Called by the pool when locking a connection for use.
+        """
+        await self.lock.acquire()
+
+    def release(self):
+        """
+        Called once the connection is no longer needed to release back into
+        the pool.
+        """
+        self.lock.release()
+        self.pool_available.release()
+
+    def locked(self):
+        """
+        Check if the connection is in use.
+        """
+        return self.lock.locked()
+
+    def close(self):
+        """
+        Close a connection (will trigger a reconnect next time the Connection)
+        is retrieved from the pool and used.
+        """
+        if self.writer:
+            self.writer.close()
+        self.writer = None
+        self.reader = None
+
+class Pool:
+    """
+    A connection pool for a single host and port. It allows up to conn_limit
+    connections to a single host at once.
+    """
+    def __init__(self, host, port, conn_limit, loop):
+        self.conn_limit = conn_limit
+
+        self.host = host
+        self.port = port
+
+        self.loop = loop
+
+        self.pool = []
+        self.pool_available = asyncio.Semaphore(self.conn_limit, loop=loop)
+        self.pool_lock = asyncio.Lock(loop=loop)
+
+    async def connect(self):
+        """
+        Waits for an available connection and then returns a connection object
+        ready to use.
+        """
+        await self.pool_available.acquire()
+
+        c = None
+
+        async with self.pool_lock:
+            if len(self.pool) < self.conn_limit:
+                c = Connection(self.host, self.port, self.pool_available, self.loop)
+                await c.acquire()
+                self.pool.append(c)
+            else:
+                for i, connection in enumerate(self.pool):
+                    if not connection.locked():
+                        await connection.acquire()
+                        c = connection
+                        break
+
+        return c
+
+    async def stats(self):
+        """
+        Count how many times each Connection object reconnected to determine
+        pool efficiency.
+        """
+        connections = 0
+
+        async with self.pool_lock:
+            for connection in self.pool:
+                connections += connection.connect_count
+
+        return connections
