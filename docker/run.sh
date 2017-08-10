b8ab8e5a891601db7ab0a1b3731440e34cabea7c
Justin Barrick <justin.m.barrick@gmail.com>
2017-08-10 08:10:09 +0000

    Add coverage report support

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,5 @@ nginx -c /uvhttp/docker/nginx.conf
 python3 ./docker/http_test_server.py
 
 python setup.py install
-nosetests -v -s 
+nosetests -v -s --with-coverage --cover-xml-file=/build_results/coverage.xml --cover-xml 
 ./uvhttp.py
